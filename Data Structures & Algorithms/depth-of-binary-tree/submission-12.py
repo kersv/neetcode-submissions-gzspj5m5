7c3f4428b7550d377a2870256d6d112ec74263ae
kersv <74420819+kersv@users.noreply.github.com>
2026-04-16 12:18:57 -0400

    Add: depth-of-binary-tree - submission-12

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-12.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # BFS Breath First Search
+        if not root: 
+            return 0
+        
+        queue = deque([(root, 1)])
+        maxDep = 0
+        while queue:
+            node, depth = queue.popleft()
+            print(node, depth)
+            maxDep = max(maxDep, depth)
+
+            if node.left:
+                queue.append([node.left,depth+1])
+            if node.right:
+                queue.append([node.right,depth+1])
+        
+        return maxDep
