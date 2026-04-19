5536d28246c221f88f35e2eab56743f2f19ef19e
kersv <74420819+kersv@users.noreply.github.com>
2026-04-19 01:05:11 -0400

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # recursive DFS
+        # no node
+        if not root:
+            return True
+
+        
+        # recursively get height of left and right side, and compare at curr node if it is balanced 
+        # compare left and right height and return True if they are within 1 height differance or equal
+        def dfs(curr):
+            if not curr:
+                return 0
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+
+            if left == -1:
+                return -1
+            if right == -1:
+                return -1
+            diff = abs(left - right)
+            
+            if diff > 1:
+                return -1
+            
+            return 1+ max(left,right)
+        
+        # call recursively, res -1 is False any other height is true
+        res = dfs(root)
+        if res == -1:
+            return False
+        return True
