a3e485765f9adb71e77ed3bd1f223a7638a6f354
kersv <74420819+kersv@users.noreply.github.com>
2026-04-19 15:18:37 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        
+
+        #  search function, find node that matchs p or q and find if the child of the node is p or q and return the curr node
+        curr = root
+        while curr:
+            # if curr.left.val == p.val or curr.right.val == q.val:
+            #     return curr
+            if curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            elif curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            else:
+                return curr
+                
+            
+            
+
+
+        
