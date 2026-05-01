69b3cd9fdee6a16cbe33e8f646864a2257e43b0a
kersv <74420819+kersv@users.noreply.github.com>
2026-04-30 20:11:07 -0400

    Add: last-stone-weight - submission-10

---
+++ Data Structures & Algorithms/last-stone-weight/submission-10.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # recursion
+
+        maxHeap = [-s for s in stones]
+
+        heapq.heapify(maxHeap)
+        
+        def rec(rocks):
+            if len(rocks) <= 1:
+                return abs(rocks[0]) if rocks else 0
+            
+
+            firstStone = heapq.heappop(rocks)
+            secondStone = heapq.heappop(rocks)
+
+            diff = firstStone - secondStone
+            heapq.heappush(rocks, diff)
+
+            return rec(rocks)
+
+        return rec(maxHeap)
+
+
+        
+
+
+        
+
